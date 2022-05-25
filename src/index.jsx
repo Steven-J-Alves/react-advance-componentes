825c3b739f1615d3e99ec0e377f334782bd8d169
Steven Alves <steven.alves@advectus.net>
2022-05-25 13:08:10 -0100

    update client rendering api

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,8 @@
-import { render } from "react-dom";
-import { App } from "./App";
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { App } from './App';
 
-render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <App />,
+);
