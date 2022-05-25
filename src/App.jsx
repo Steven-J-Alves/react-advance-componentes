4aad94f3b00ec0fe573fc61e2a31c15939c6726c
Steven Alves <steven.alves@advectus.net>
2022-05-25 13:08:20 -0100

    import styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import './styles/global.scss'
+import './styles/custom.css';
+import '../dist/style/tailwind/tailwindBuild.css';
 
 export function App() {
-  return <h1>app</h1>
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
