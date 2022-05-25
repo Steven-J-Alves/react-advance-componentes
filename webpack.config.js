c57933795e6eab6aff260c65f3c48137c53e1d8d
Steven Alves <steven.alves@advectus.net>
2022-05-25 10:43:13 -0100

    fix devserver and add devtool

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin') 
 
+const isDevelopment = process.env.NODE_ENV !== 'production' 
+
 module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map': 'source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -25,7 +29,9 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'public')
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
   },
-  mode: 'development',
 }
+
